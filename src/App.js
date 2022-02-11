1053a4263eec376626fd27c94fe603536a5051d0
sokolovskaya-oli <sokolovskaya.oli@gmail.com>
2022-02-11 15:56:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,39 +5,74 @@ import TotalTable from "./Components/TotalTable";
 
 
 function App() {
-  let countEatingPizza = 0;
   
-   const apiUrlGetGuests = 'https://gp-js-test.herokuapp.com/pizza/guests';
-  const apiUrlGetVegans = `https://gp-js-test.herokuapp.com/pizza/world-diets-book/{name}`;
-  const apiUrlGetCurrency = 'https://gp-js-test.herokuapp.com/pizza/currency';
-  // const apiUrlGetDrink = `https://gp-js-test.herokuapp.com/pizza/order-cola/${countEatingPizza}`;
-  // const apiUrlGetPizza = `https://gp-js-test.herokuapp.com/pizza/order/${orderPizza}/${countEatingPizza}`
-  
-  const [guests, setGuests]=useState([])
-  const [vegan, setVegan]=useState([])  
-  const [eaters, setEaters]= useState([])
  
-    useEffect(() => {
-       fetch(apiUrlGetGuests)
-        .then((response) => response.json())
-        .then((json) => setGuests(json.party))
-      // .catch((error) => alert(error))
-    }, [setGuests])
+  let namesArr= []
+ 
+   const apiUrlGetGuests = 'https://gp-js-test.herokuapp.com/pizza/guests';
+   const apiUrlGetCurrency = 'https://gp-js-test.herokuapp.com/pizza/currency';
+  // const apiUrlGetDrink = `https://gp-js-test.herokuapp.com/pizza/order-cola/${countEatingPizza}`;
+  //
+    const [guests, setGuests]=useState([])
+    const [vegan, setVegan]=useState([])  
+  //const [eaters, setEaters]= useState([])
+  //const [name, setName]= useState([])
 
-    useEffect(() => {
-      fetch(apiUrlGetVegans)
-        .then((res) => res.json())
-        .then((repos) => setVegan(repos.party));
-      }, [setVegan])
-   
+  function Names(){
+    guests.forEach(i=>namesArr.push(i.name))
+   return namesArr
+  }
 
+  function getPizzaType(){
+    let pizzaTypesVegan = ['vegan', 'cheese']
+    if(vegan.length > guests.length / 100 * 50 ){
+      let randomNum = Math.floor(Math.random() * 2)
+         return pizzaTypesVegan[randomNum]
+    }else{
+         return 'meat'
+    }
+  }
+  const loadGuests = async()=>{
+    const response = await fetch(apiUrlGetGuests);
+    const data = await response.json()
+    setGuests(data.party.filter(note => note.eatsPizza === true ))
+  }
+  let link = Names().toString().replace(/ /ig, '%20')
+  const apiUrlGetVegans = `https://gp-js-test.herokuapp.com/pizza/world-diets-book/${link}`;
+  
+  const loadVegans = async()=>{
+    const response = await fetch(apiUrlGetVegans);
+    const data = await response.json()
+    setVegan(data.diet.filter(({isVegan})=> isVegan === true))
+  }
 
-// Promise.all([
-//   httpGet('/article/promise/user.json'),
-//   httpGet('https://gp-js-test.herokuapp.com/pizza/currency')
-// ]).then(results => {
-//   console.log(results);
-// });
+  async function Currensy () {
+    let response = await fetch(apiUrlGetCurrency)
+    let json1 = await response.json()
+    return json1
+    }
+          
+  async function Order() {
+      const apiUrlGetPizza = `https://gp-js-test.herokuapp.com/pizza/order/${getPizzaType()}/12`
+      let response = await fetch(apiUrlGetPizza)
+      let json2 = await response.json()
+      return json2
+      }
+
+  let promises=[Currensy(), Order()]   
+
+  useEffect(() => {
+         loadGuests();  
+         loadVegans();
+        }, []) 
+ 
+
+  
+  
+  Promise.all(promises)
+    .then(results => {
+   console.log(results);
+ });
 
   return (
     <div className="App">
